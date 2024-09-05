6370e285dcd13472851c13d577c6a8216e5ee0af
mimas9107 <mimas9107@gmail.com>
2024-09-05 22:00:22 +0800

    modified:   hellocv.cpp

--- hellocv.cpp
+++ hellocv.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 using namespace cv;
 int main(int argc, char* argv[]){
+    if(argc==1) return 0L;
     Mat img = imread(argv[1], IMREAD_GRAYSCALE);
     if (!img.data){
         return -1;
@@ -21,21 +22,21 @@ int main(int argc, char* argv[]){
     std::cout << "dim= " << img.dims << endl;
     std::cout << "======================" << endl;
 
-    for(int r=0; r < img.rows; r++){
-        for(int c=0; c < img.cols; c++){
-            std:cout << img.at<int>(r,c) << ",";
-        }
-        cout << endl;
-    }
+    // for(int r=0; r < img.rows; r++){
+    //     for(int c=0; c < img.cols; c++){
+    //         std:cout << img.at<int>(r,c) << ",";
+    //     }
+    //     cout << endl;
+    // }
     
-    for(int r=0; r<img.rows; r++){
-        const int *ptr = img.ptr<int>(r);
-        for(int c=0; c<img.cols; c++){
-            std::cout << ptr[c] << ";";
+    // for(int r=0; r<img.rows; r++){
+    //     const int *ptr = img.ptr<int>(r);
+    //     for(int c=0; c<img.cols; c++){
+    //         std::cout << ptr[c] << ";";
 
-        }
-        std::cout << endl;
-    }
+    //     }
+    //     std::cout << endl;
+    // }
 
     imshow("orig", img);
     waitKey(0);
